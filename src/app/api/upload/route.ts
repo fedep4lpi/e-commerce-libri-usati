14f3f375a36638597a7f0d147eb6a24c5b4d3d30
fedep4lpi <federicopalpi@gmail.com>
2023-09-27 23:35:03 +0200

    file upload adedd

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,20 +1,27 @@
-import { writeFile } from 'fs/promises'
 import { NextRequest, NextResponse } from 'next/server'
+import { v4 as uuidv4 } from 'uuid'
+import sharp from 'sharp'
 
 export async function POST(request: NextRequest) {
-  const data = await request.formData()
-  const file: File | null = data.get('file') as unknown as File
 
-  if (!file) {
-    return NextResponse.json({ success: false })
+  try {
+
+    const data = await request.formData()
+    const file: File | null = data.get('file') as unknown as File
+
+    if (!file) {
+      return NextResponse.json({ success: false })
+    }
+
+    const bytes = await file.arrayBuffer()
+    const buffer = Buffer.from(bytes)
+
+    await sharp(buffer).webp().toFile(`./public/images/${uuidv4()}.webp`)
+
+  } catch (err) {
+    console.log(err)
+    return NextResponse.json({ message: err })
   }
-
-  const bytes = await file.arrayBuffer()
-  const buffer = Buffer.from(bytes)
-
-  const path = `/tmp/${file.name}`
-  await writeFile(path, buffer)
-  console.log(`open ${path} to see the uploaded file`)
 
   return NextResponse.json({ success: true })
 }
--- src/app/sell/page.tsx
+++ src/app/sell/page.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+'use client'
 
-const page = () => {
+import React, { useState } from 'react'
+
+const Sell = () => {
+
+  const [file, setFile] = useState<File>()
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+
+    try {
+
+      e.preventDefault()
+
+      console.log(1)
+
+      if (!file) return
+
+      console.log(2)
+
+      const data = new FormData()
+      data.set('file', file)
+
+      const res = await fetch('/api/upload', {
+        method: 'POST',
+        body: data
+      })
+
+      console.log(3)
+
+      // handle the error
+      if (!res.ok) throw new Error(await res.text())
+      
+    } catch (e: any) {
+
+      // Handle errors here
+      console.error(e)
+
+    } finally {
+      // @ts-ignore
+      document.querySelector('#fileInput').value = null
+    }
+  }
+
   return (
-    <div>page</div>
+    <main>
+      <form onSubmit={onSubmit}>
+        <input
+          type="file"
+          accept='.png,.jpg,.jpeg,.heic,.webp'
+          name="file"
+          id='fileInput'
+          onChange={(e) => setFile(e.target.files?.[0])}
+        />
+        <button type='submit'>Invia</button>
+      </form>
+    </main>
   )
 }
 
-export default page
+export default Sell

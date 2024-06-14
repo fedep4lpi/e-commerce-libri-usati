e56b01d1c6f6afcfeb14418f29ec1974de8599f5
fedep4lpi <federicopalpi@gmail.com>
2024-06-14 11:52:40 +0200

    fixed data license src

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -4,7 +4,7 @@ import IODLLogo from '@/icons/IODL_logo.svg'
 export default function Home() {
   return (
     <main className=' w-full h-full flex justify-center items-center'>
-      applicazione sviluppata grazie ai dati di <a href="http://dati.istruzione.it" className=' text-blue-700 px-1'>dati.istruzione.it </a> con
+      applicazione sviluppata grazie ai dati di <a href="http://dati.istruzione.it" className=' text-blue-700 px-1' target="_blank">dati.istruzione.it </a> con
       <a href="https://www.dati.gov.it/content/italian-open-data-license-v20" target="_blank">
         <Image src={IODLLogo} alt='IODL logo'/>
       </a>

7e323ddb313c4d7ce42817be7210114b7c9cb616
fedep4lpi <federicopalpi@gmail.com>
2024-06-14 12:01:08 +0200

    added ui interface to send messages

--- src/app/(pages)/inbox/[contact]/page.tsx
+++ src/app/(pages)/inbox/[contact]/page.tsx
@@ -1,10 +1,11 @@
 'use client'
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import { decode } from "urlencode"
 import { MessageType } from './types'
 import Message from "./message"
 import Image from 'next/image'
 import sendIcon from "../../../../icons/send.svg"
+import useAutosizeTextArea from "./useAutosizeTextArea"
 
 export default function Page({ 
   params 
@@ -15,29 +16,59 @@ export default function Page({
   params.contact = decode(params.contact)
 
   const [ messages, setMessages ] = useState<Array<MessageType>>([])
+  const [value, setValue] = useState("");
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
-  const fetchMessages = async () => {
+  useAutosizeTextArea(textAreaRef.current, value);
+
+  const handleChange = (evt: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const val = evt.target?.value;
+    setValue(val);
+  }
+
+  const handleSubmit = async () => {
     try {
-      const res = await fetch('/api/messages/getmessages', {
+      const res = await fetch('/api/messages/sendmessage', {
         method: 'POST',
         body: JSON.stringify({
-          contact: params.contact
+          receiver: params.contact,
+          content: value
         })
       })
       console.log(res)
-      const data = (await res.json()).messages
-      setMessages(data)
     } catch(err) {
       console.log(err)
     }
-  }  
+  }
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(()=>{fetchMessages()}, [])
+  useEffect(()=>{
+
+    const fetchMessages = async () => {
+      try {
+        const res = await fetch('/api/messages/getmessages', {
+          method: 'POST',
+          body: JSON.stringify({
+            contact: params.contact
+          })
+        })
+        console.log(res)
+        const data = (await res.json()).messages
+        setMessages(data.reverse())
+      } catch(err) {
+        console.log(err)
+      }
+    }  
+
+    const fetchTimeout = setTimeout(fetchMessages, 600)
+    return () => clearTimeout(fetchTimeout)
+  }, 
+  [messages, params.contact])
 
   return (
     <main className=" w-full h-full py-10 px-40 flex flex-col justify-between">
-      <div>
+      <div
+        className=" w-full h-full overflow-y-scroll my-8 pr-3 first:mt-0 flex flex-col-reverse"
+      >
         {
           //@ts-ignore
           messages.map((mess, idx) => {
@@ -48,11 +79,22 @@ export default function Page({
         }
       </div>
       <div className="  p-6 w-full bg-violet-700 rounded-lg flex">
-        <textarea name="" id="" className="w-full rounded-l-lg h-8 pl-2"></textarea>
-        <button className=" bg-blue-700 px-5 rounded-r-lg">
+        <textarea
+          id="review-text"
+          onChange={handleChange}
+          placeholder="Enter the message"
+          ref={textAreaRef}
+          rows={1}
+          value={value}
+          className=" w-full rounded-lg p-3"
+        />
+        <button 
+          className=" bg-blue-700 h-11 px-3 rounded-full ml-5 mt-auto"
+          onClick={handleSubmit}
+        >
           <Image src={sendIcon} alt="send"/>
         </button>
-      </div>
+      </div> 
     </main>   
   )
 }
